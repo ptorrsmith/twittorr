c39c64851ee4f9f86e0008f868e70c068cbe2179
Peter Torr Smith <peter.torr.smith@cultureamp.com>
2021-12-16 14:56:39 +1300

    Users component with gpl embedded query, interface for typing

---
+++ web/src/components/Users.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+const USERS_QUERY = gql`
+query USERS_QUERY {
+  users {
+    id
+    name
+  }
+}
+`
+
+interface User {
+  id: string;
+  name: string;
+}
+
+export default function Users() {
+
+  const { loading, error, data } = useQuery(USERS_QUERY)
+
+  if (loading) return <p>Loading...</p>
+
+  if (error) return <p>Error :( "{error.message}"</p>
+
+  return (
+    <div className="users">
+      <p>Hi</p>
+      {data.users.map((user: User) => (
+        <div key={user.id}>
+          <h2>{user.name}</h2>
+        </div>
+          )
+        )
+      }
+    </div>
+  );
+}
