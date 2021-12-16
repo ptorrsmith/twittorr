b3b47b638872e3c34d4718d46603254286324902
Peter Torr Smith <peter.torr.smith@cultureamp.com>
2021-12-16 15:54:26 +1300

    add, import and use React Router Dom V6 with `yarn add react-router-dom`. Note V6 includes Types :-)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import React from 'react'
 import './App.css'
 import Users from './components/Users'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
@@ -11,9 +12,15 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="Test">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Users />} />
+        </Routes>
+      </Router>
+
+      {/* <div className="Test">
         <Users />
-      </div>
+      </div> */}
     </ApolloProvider>
   );
 }
